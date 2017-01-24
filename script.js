926a8b5d913a222dc6b33e37953450d23843e9e3
Alan <alanpieczonka@gmail.com>
2017-01-24 13:35:52 +0100

    Basic JSON config

--- script.js
+++ script.js
@@ -2,4 +2,52 @@ console.log("This shit works!");
 
 $(document).ready(function(){
    console.log("This other one works too!"); 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    var ourRequest = new XMLHttpRequest();
+    
+    ourRequest.open('GET', 'http://api.openweathermap.org/data/2.5/weather?lat=49.7249238&lon=19.1004921&appid=c1b77262ffa7f5e4ad709e332c64bbfa');
+    
+    ourRequest.send();
+    
+    ourRequest.onload = function(){
+      if(ourRequest.status >= 200 && ourRequest.status < 400){
+         var ourData = JSON.parse(ourRequest.responseText);
+        renderHTML(ourData);
+    }
+        
+        
+        
+    else{
+        console.log("we connected to the server, but it returned and error!");
+    }  
+}
+    
+    ourRequest.onerror = function(){
+        console.log("Connection lost. Please visit our FAQ to get more info.");
+    }
+    
+    
+    
+    
 });
+
+
+function renderHTML(data){
+    console.log("render działa");
+    var htmlString = "";
+    
+    htmlString += data.coord.lon + " " + data.name + " " + data.sys.country;
+        
+    console.log(htmlString);
+}
